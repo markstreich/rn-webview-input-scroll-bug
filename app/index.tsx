df53f9c46770d55f451a22fd6d4ee1d394f24003
Mark <drgllen@gmail.com>
2025-02-18 08:21:34 +0200

    no resize example

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,18 @@
-import {Button, Keyboard, View} from "react-native";
+import { Button, Keyboard, ScrollView, View } from "react-native";
 import WebView from "react-native-webview";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
-    const [keyboardOffset, setKeyboardOffset] = useState(0);
-
-    useEffect(() => {
-        const keyboardShowEvent = Keyboard.addListener('keyboardDidShow', (e) => {
-            setKeyboardOffset(e.endCoordinates.height);
-        });
-
-        const keyboardHideEvent = Keyboard.addListener('keyboardDidHide', () => {
-            setKeyboardOffset(0);
-        });
-
-        return () => {
-            keyboardShowEvent.remove();
-            keyboardHideEvent.remove();
-        };
-    }, []);
-
-    return (
-        <View style={{backgroundColor: 'red', flex: 1}}>
-            <View style={{flex: 1, paddingBottom: keyboardOffset}}>
-                <WebView source={require('../assets/index.html')} scrollEnabled={true}/>
-            </View>
-            <Button title="Toggle" onPress={() => {
-                setKeyboardOffset((x) => x ? 0 : 350);
-            }}/>
-        </View>
-    );
+  return (
+    <ScrollView style={{ backgroundColor: "red", flex: 1, height: "100%" }}>
+      <View style={{ height: 700, backgroundColor: "green" }} />
+      <View style={{ flex: 1, height: 300, backgroundColor: "blue" }}>
+        <WebView
+          source={require("../assets/index.html")}
+          scrollEnabled={true}
+        />
+      </View>
+      <View style={{ height: 700, width: "100%", backgroundColor: "green" }} />
+    </ScrollView>
+  );
 }
